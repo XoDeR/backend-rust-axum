599f1bea84c4f7bc23f91a861675617062efcdd9
XoDeR <xoder.vs@gmail.com>
2025-12-29 17:34:53 +0100

    loading settings from .env file

--- crates/bin/src/main.rs
+++ crates/bin/src/main.rs
@@ -1,4 +1,13 @@
+use core::config::AppConfig;
+use std::sync::Arc;
+use clap::Parser;
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    dotenv::dotenv().ok();
+    let config = Arc::new(AppConfig::parse());
+    
     println!("Nexus backend is running...");
+
+    Ok(())
 }
